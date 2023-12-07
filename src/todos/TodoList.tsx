80bffa04b61e6e1864cf2fd36d275e89fc845c0e
Anton Pavliuk <streetreadger@gmail.com>
2023-12-07 12:18:35 +0200

    Add styles, finish putting all together

--- src/todos/TodoList.tsx
+++ src/todos/TodoList.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import NewTodoForm from "./NewtodoForm";
 import TodoListItem from './TodoListItem';
 import './TodoList.css';
 
-const TodoList = ({ todos }) => (
+const TodoList = ({ todos = [{ text: 'Hello'}] }) => (
     <div className="list-wrapper">
+        <NewTodoForm/>
         {
             todos.map(todo => <TodoListItem todo={todo}/>)
         }
