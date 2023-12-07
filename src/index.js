8979625c1daabd1889bb1884e252c367829ff838
Anton Pavliuk <streetreadger@gmail.com>
2023-12-07 13:02:44 +0200

    Add redux old configuration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
-import { createRoot } from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import App from './App';
+import {Provider} from "react-redux";
+import {configureStore} from "./store";
 
 createRoot(document.getElementById('root'))
-.render(<App />);
+    .render(
+        <Provider store={configureStore()}>
+            <App/>
+        </Provider>
+    );
